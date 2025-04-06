c08ba381701cb2ce3e7e05d5d6c2a6f91e067630
Antonina Danylchenko <antonina.ivanets@gmail.com>
2025-04-06 19:07:09 +0200

    change: the duplicated code

--- app/main.py
+++ app/main.py
@@ -11,13 +11,7 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            } for error in errors
+            format_linter_error(error) for error in errors
         ],
         "path": file_path,
         "status": "passed" if len(errors) == 0 else "failed"
@@ -25,16 +19,4 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [{
-        "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            } for error in errors
-        ],
-        "path": file_path,
-        "status": "passed" if len(errors) == 0 else "failed"
-    } for file_path, errors in linter_report.items()]
+    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
